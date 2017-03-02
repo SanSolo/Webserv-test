99320350c73f875770e10009d8c15cbbc05031fa
Nils Lehmann <lehmannnil@gmail.com>
2017-03-02 16:03:26 +0100

    Bonus: Issues list is paginated

--- routes/issues.js
+++ routes/issues.js
@@ -53,17 +53,33 @@ const User = require('../models/user');
 
  // GET /issues
  router.get('/', function(req, res, next) {
-   let query = Issue.find();
-   // Filter by status
-   if (req.query.status && req.query.status != "") {
-     query = query.where('status').equals(req.query.status);
-   }
-   // Execute the query
-   query.exec(function(err, issues) {
-     if (err) {
-       return next(err);
+   Issue.find().count(function(err, total) {
+     if (err) { return next(err); };
+     let query = Issue.find();
+     console.log(total);
+     // Parse the "page" param (default to 1 if invalid)
+     let page = parseInt(req.query.page, 10);
+     if (isNaN(page) || page < 1) { /* ... */ }
+     // Parse the "pageSize" param (default to 100 if invalid)
+     let pageSize = parseInt(req.query.pageSize, 10);
+     if (isNaN(pageSize) || pageSize < 0 || pageSize > 100) { /* ... */ }
+     // Apply skip and limit to select the correct page of elements
+     query = query.skip((page - 1) * pageSize).limit(pageSize);
+     res.set('Pagination-Page', page);
+     res.set('Pagination-PageSize', pageSize);
+     res.set('Pagination-Total', total);
+
+     // Filter by status
+     if (req.query.status && req.query.status != "") {
+       query = query.where('status').equals(req.query.status);
      }
-     res.send(issues);
+     // Execute the query
+     query.exec(function(err, issues) {
+       if (err) {
+         return next(err);
+       }
+       res.send(issues);
+     });
    });
  });
 
